e1a15cb59e5d4c0e463d0db26e43b054d34bca81
Matt_Windows <218104632+mabstract25@users.noreply.github.com>
2026-01-15 10:16:25 +0000

    Build createGrid function

---
+++ etchasketch.js
@@ -0,0 +1,16 @@
+let container = document.getElementById('container')
+
+window.onload = createGrid(16);
+
+function createGrid(n){
+    for(let row = 0; row<n; row++){
+        for(let column = 0; column<n; column++){
+            let div1 = document.createElement("div");
+            div1.classList.add("grid");
+            div1.style.background = "pink";
+            div1.style.width = `${800/n}px`;
+            div1.style.height = `${800/n}px`;
+            container.appendChild(div1);
+        }
+    }
+}
